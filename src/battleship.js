170526356960ddb33613a4d92572447339d2ca34
Dylan Hans <z5726279@ad.unsw.edu.au>
2026-01-22 23:07:52 +1100

    feat: hit() and isSunk() on Ship

--- src/battleship.js
+++ src/battleship.js
@@ -1,8 +1,18 @@
 class Ship {
+  #hits = 0;
+
   constructor(position, length, vertical) {
     this.position = position;
     this.length = length;
     this.vertical = vertical;
+  }
+
+  hit() {
+    this.#hits++;
+  }
+
+  isSunk() {
+    return this.#hits >= this.length;
   }
 }
 
