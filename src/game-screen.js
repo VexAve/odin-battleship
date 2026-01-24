9622d8c6832e0d403f96a40097291ef24cfeb1b4
Dylan Hans <z5726279@ad.unsw.edu.au>
2026-01-24 22:29:03 +1100

    feat: display empty gameboard grid

--- src/game-screen.js
+++ src/game-screen.js
@@ -1,6 +1,28 @@
-export default () => {
+const loadPlayerGrid = () => {
   const content = document.createElement("div");
-  content.className = "game-screen";
+
+  const grid = document.createElement("div");
+  content.appendChild(grid);
+  grid.className = "grid";
+
+  const cells = [];
+  for (let i = 0; i < 10; i++) {
+    cells.push([]);
+    for (let j = 0; j < 10; j++) {
+      cells[i].push(document.createElement("div"));
+      grid.appendChild(cells[i][j]);
+      cells[i][j].className = "cell";
+    }
+  }
 
   return content;
-}
+};
+
+export default () => {
+  const content = document.createElement("div");
+  content.id = "game-screen";
+
+  content.appendChild(loadPlayerGrid());
+
+  return content;
+};
