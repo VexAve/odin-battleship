f67c1b72c813ac6e35ab74ffb5283642790ad390
Dylan Hans <z5726279@ad.unsw.edu.au>
2026-01-27 23:53:46 +1100

    refactor: used CSS and className to style the tiles

--- src/game-screen.js
+++ src/game-screen.js
@@ -1,25 +1,15 @@
 const displayGrid = (gameboard, cells) => {
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
-      if (gameboard.grid[i][j].shipIndex === -1) {
-        if (gameboard.grid[i][j].status === "miss") {
-          cells[i][j].style.backgroundColor = "lightgrey";
-          cells[i][j].textContent = "•";
-          cells[i][j].style.color = "darkgrey";
-        } else if (gameboard.grid[i][j].status === "flag") {
-          cells[i][j].style.backgroundColor = "darkgrey";
-          cells[i][j].textContent = "•";
-          cells[i][j].style.color = "grey";
-        }
-      } else {
-        cells[i][j].style.backgroundColor = "lightblue";
-        if (gameboard.grid[i][j].status === "hit") {
-          cells[i][j].style.backgroundColor = "pink";
-          cells[i][j].textContent = "✖";
-          cells[i][j].style.color = "red";
-          if (gameboard.placedShips[gameboard.grid[i][j].shipIndex].isSunk()) {
-            cells[i][j].style.borderColor = "red";
-          }
+      cells[i][j].className =
+        gameboard.grid[i][j].shipIndex > -1
+          ? "cell show-ship"
+          : "cell " + gameboard.grid[i][j].status;
+      if (gameboard.grid[i][j].status === "hit") {
+        cells[i][j].className = "cell hit";
+        cells[i][j].textContent = "✖";
+        if (gameboard.placedShips[gameboard.grid[i][j].shipIndex].isSunk()) {
+          cells[i][j].classList.add("sunk");
         }
       }
     }
@@ -45,6 +35,7 @@ const loadPlayerGrid = (player, gameboard, currentTurn) => {
       cells[i].push(document.createElement("div"));
       grid.appendChild(cells[i][j]);
       cells[i][j].className = "cell";
+      cells[i][j].textContent = "•";
 
       if (currentTurn) {
         cells[i][j].addEventListener("click", () => {
@@ -65,8 +56,8 @@ export default (players, gameboards, firstPlayerTurn) => {
   content.id = "game-screen";
 
   const playerGrids = document.createElement("div");
-  playerGrids.id = "player-grids";
   content.appendChild(playerGrids);
+  playerGrids.id = "player-grids";
 
   playerGrids.appendChild(
     loadPlayerGrid(players[0], gameboards[0], firstPlayerTurn),
